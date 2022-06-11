597ea57f87a0a52a27c0c8e9570ed2a7783df30e
ppantazos <panos@schoox.com>
2022-06-11 14:31:19 +0300

    master

--- client/src/layouts/MenuLayouts/SidebarMenu.tsx
+++ client/src/layouts/MenuLayouts/SidebarMenu.tsx
@@ -1,39 +1,59 @@
-import React from 'react';
+import React, { Children } from 'react';
 import {Menu} from 'antd';
 import './index.css'
-import {defaultMenuItems, projectMenuItems} from './SidebarMenuItems';
+import { projectMenuItems, sidemenuItems, sideMenuItems} from './SidebarMenuItems';
 import {Link, useLocation} from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import { ItemType } from 'antd/lib/menu/hooks/useItems';
 
 function SidebarMenu(): JSX.Element {
     const location = useLocation();
     let params:{projectName:string} = useParams();
 
-    let items
-    let menu_type:Number
-    if(location.pathname.includes('/project/')){
-        items = projectMenuItems
-        // todo kapws na doulepsei na valw sto menu to id ap to project gia to navigation
-        menu_type = 1
-    }else{
-        items = defaultMenuItems
-        menu_type = 0
-    }
+    let antd_menu:ItemType[] = []
+
+    let items = sideMenuItems
+    // let menu_type:Number
+    // if(location.pathname.includes('/project/')){
+    //     items = projectMenuItems
+    //     // todo kapws na doulepsei na valw sto menu to id ap to project gia to navigation
+    //     menu_type = 1
+    // }else{
+        // items = sidemenuItems
+        // items = sideMenuItems
+        // menu_type = 0
     
+    items.map((item, key)=>{
+        if(item.has_access){
+            let temp_item = { 
+                label: item.href?<a href={item.href}>{item.label}</a>:item.label,
+                icon: item.icon,
+                key: key.toString(),
+                children: item.children
+            }
+            
+            antd_menu.push(temp_item)
+        }
+    })
+
     const index = items.findIndex((menuItem) => menuItem.href === location.pathname)
 
-    
     return (
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={[index.toString()]}>
-            {
+        <Menu 
+            theme="dark" 
+            mode="inline" 
+            defaultSelectedKeys={[index.toString()]}
+            items={antd_menu}
+            >
+            {/* {
                 items.map((menuItem, index) => (
                     <Menu.Item key={index} icon={menuItem.icon}>
-                        <Link to={(menu_type===1 && menuItem.title==='Project')?menuItem.href + '/' + params.projectName:menuItem.href}>
-                            {menuItem.title}
-                        </Link>
-                    </Menu.Item>
-                ))
-            }
+                        {/* <Link to={(menu_type===1 && menuItem.label==='Project')?menuItem.href + '/' + params.projectName:menuItem.href}>
+                            {menuItem.label}
+                        </Link> */}
+                    {/* </Menu.Item> */}
+                {/* )) */}
+            {/* } */} 
         </Menu>
     );
 }
--- client/src/layouts/MenuLayouts/SidebarMenuItems.tsx
+++ client/src/layouts/MenuLayouts/SidebarMenuItems.tsx
@@ -1,8 +1,12 @@
-import { ApartmentOutlined, BarChartOutlined, CalendarOutlined, CheckSquareFilled, ClusterOutlined, CoffeeOutlined, DashboardFilled, SettingOutlined, TagFilled, UserOutlined } from "@ant-design/icons";
+import { ApartmentOutlined, BarChartOutlined, CalendarOutlined, PlayCircleOutlined, ClusterOutlined, DashboardFilled, SettingOutlined, TagFilled } from "@ant-design/icons";
 import { BackwardFilled } from '@ant-design/icons';
-
 import React from "react";
 
+
+/*
+    fetch Projects
+    fetch user role
+*/
 
 export type MenuItem = {
     title: string,
@@ -10,59 +14,82 @@ export type MenuItem = {
     icon: JSX.Element
 };
 
+export type sidemenuItems = {
+    label: string,
+    href?: string,
+    icon: JSX.Element,
+    has_access: boolean,
+    children?: {
+        label: string,
+        href: string
+    }[]
+}
 
-export const defaultMenuItems: MenuItem[] = [
+export const sideMenuItems: sidemenuItems[] = [
     {
-        title: 'Overview',
+        label: 'Overview',
         href: '/overview',
-        icon: <DashboardFilled/>
+        icon: <DashboardFilled/>,
+        has_access: true
     },
     {
-        title: 'Test Suites',
-        href: '/test-suites',
-        icon: <CheckSquareFilled/>
+        label: 'Projects', 
+        icon: <ApartmentOutlined />,
+        has_access: true,
+        children: [{
+            label: 'Project 1',
+            href: 'TODO/SOMEWHERE'
+        }, {
+            label: 'Project 2',
+            href: 'TODO/SOMEWHERE'
+        }]
     },
     {
-        title: 'Tags',
+        label: 'Run',
+        href: '/run',
+        icon: <PlayCircleOutlined />,
+        has_access: true
+    },
+    {
+        label: 'Tags',
         href: '/tags',
-        icon: <TagFilled/>
+        icon: <TagFilled/>,
+        has_access: true
     },
     {
-        title: 'Users',
-        href: '/users',
-        icon: <UserOutlined />
-    },
-    {
-        title: 'Calendar',
-        href: '/calendar',
-        icon: <CalendarOutlined />
-    },
-    {
-        title: 'Admin',
-        href: '/panel',
-        icon: <SettingOutlined />
-    },
-    {
-        title: 'Jobs',
-        href: '/panel/jobs',
-        icon: <CoffeeOutlined />
-    },
-    {
-        title: 'Teams',
+        label: 'Teams',
         href: '/panel/teams', ///'/panel/jobs',
-        icon: <ClusterOutlined />
+        icon: <ClusterOutlined />,
+        has_access: true
     },
     {
-        title: 'Projects',
-        href: '/projects', 
-        icon: <ApartmentOutlined />
+        label: 'Calendar',
+        href: '/calendar', 
+        icon: <CalendarOutlined />,
+        has_access: true
     },
     {
-        title: 'Dashboard',
+        label: 'Dashboard',
         href: '/dashboards', 
-        icon: <BarChartOutlined />
+        icon: <BarChartOutlined />,
+        has_access: true
+    },
+    {
+        label: 'Admin',
+        icon: <SettingOutlined />,
+        has_access: true,
+        children: [{
+            label: 'Panel',
+            href: '/panel'
+        }, {
+            label: 'Jobs',
+            href: '/panel/jobs'
+        }, {
+            label: 'Users',
+            href: '/panel/users'
+        }]
     }
-];
+]
 
 
 export const projectMenuItems: MenuItem[] = [
